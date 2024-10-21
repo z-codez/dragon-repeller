e3ada2b157cf7d66d9e9a8c5760b852cd27011d1
dci-student <137389454+z-codez@users.noreply.github.com>
2024-10-21 07:29:52 +0200

    worked on more functions

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-
+/*  Variables Initializations     */
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
-
 const text = document.querySelector("#text");
 
-button1.onclick = goStore;
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\""
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You have entered the store."
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button  functions": [fightSlime, fightBeast, goTown]
+  }
+]
+
+button1.addEventListener("click", goStore);
+
+function update(location) {}
 
 function goStore() {
   text.innerText = "You enter the store.";
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
   button3.innerHTML = "Go to town square";
+
+  button1.addEventListener("click", buyHealth);
+  button2.addEventListener("click", buyWeapon);
+  button3.addEventListener("click", goTown);
 }
 
 function goCave() {
@@ -20,5 +44,19 @@ function goCave() {
 }
 
 function fightDragon() {
-  text.innerText = "Going to Town square";
+  text.innerText = "Fighting dragon";
 }
+
+function buyHealth() {}
+
+function buyWeapon() {
+  return undefined;
+}
+
+function goTown() {
+  return undefined;
+}
+
+function fightSlime() {}
+
+function fightBeast() {}
