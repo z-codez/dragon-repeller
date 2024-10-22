18cc0e6c567cd5e789f96cfbdd3451275c65e502
dci-student <137389454+z-codez@users.noreply.github.com>
2024-10-22 06:30:40 +0200

    Refactored functions to follow DRY

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+let xp = 0;
+let gold = 50;
+let health = 100;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+
 /*  Variables Initializations     */
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
+const goldText = document.querySelector("#goldText");
+const healthText = document.querySelector("#healthText");
 
 const locations = [
   {
@@ -20,42 +31,98 @@ const locations = [
   {
     name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button  functions": [fightSlime, fightBeast, goTown]
+    "button  functions": [fightSlime, fightBeast, goTown],
+    text: "You entered the cave. You see some monsters."
   }
 ]
 
-button1.addEventListener("click", goStore);
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+  {
+    name: "sword",
+    power: 100
+  }
+]
 
-function update(location) {}
+/*  INITIAL BUTTON EVENT LISTENERS  */
+button1.addEventListener("click", goStore);
+button2.addEventListener("click", goCave);
+button3.addEventListener("click", fightDragon);
+
+function update(location) {
+  text.innerText = location.text;
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerHTML = location["button text"][2];
+
+  button1.addEventListener("click", location["button functions"][0]);
+  button2.addEventListener("click", location["button functions"][1]);
+  button3.addEventListener("click", location["button functions"][2]);
+}
+
+function goTown() {
+  update(locations[0]);
+}
 
 function goStore() {
-  text.innerText = "You enter the store.";
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerHTML = "Go to town square";
-
-  button1.addEventListener("click", buyHealth);
-  button2.addEventListener("click", buyWeapon);
-  button3.addEventListener("click", goTown);
+  update(locations[1]);
 }
 
 function goCave() {
-  text.innerText = "Going to cave";
+  update(locations[2]);
 }
 
 function fightDragon() {
   text.innerText = "Fighting dragon";
 }
 
-function buyHealth() {}
+function buyHealth() {
+  if(gold >= 10) {
+    gold -= 10;
+    health += 10;
+
+    healthText.innerText = health;
+    goldText.innerText = gold;
+  } else {
+    text.innerText = "You do not have enough gold to buy health."
+  }
+}
 
 function buyWeapon() {
-  return undefined;
+  if(gold >= 30) {
+    gold -= 30;
+    currentWeaponIndex++;
+    let newWeapon = weapons[currentWeaponIndex].name;
+    inventory.push(newWeapon);
+    goldText.innerText = gold;
+    text.innerText = "You now have a " + newWeapon;
+    text.innerText += ". In your inventory you have:";
+
+    for(let i = 0; i < inventory.length; i++) {
+      let ending;
+      if(i === inventory.length - 1) {
+        ending = ".";
+      } else {
+        ending = ", ";
+      }
+
+      text.innerText += " " + inventory[i] + ending;
+    }
+  }
 }
 
-function goTown() {
-  return undefined;
-}
+
 
 function fightSlime() {}
 
